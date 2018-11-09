8d195476a432bf7325b3febeca2ad9f90184f672
Teodora Gajic-Bakic <teodora.gajic-bakic@talkinc.org>
2018-11-08 21:13:04 -0500

    added kviz.html and kviz.js

---
+++ kviz.js
@@ -0,0 +1,26 @@
+function check() {
+	var q1=document.myform.q1.value;
+	var count=0;
+
+	if(q1=="a") {
+		    count++;
+	}
+
+alert(You ROCK and you got golden "+count+" point);
+
+	var q2=document.myform.q2.value;
+	var count=0;
+
+    if(q2=="b") {
+		    count++;
+	}
+alert(You ROCK and you got golden "+count+" point);
+
+	var q3=document.myform.q3.value;
+	var count=0;
+
+	if(q3=="a") {
+		    count++;
+	}
+	alert(You ROCK and you got golden "+count+" point);
+}
